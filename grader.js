5cb543a49b71a8757b2a59120ad111f215293885
Carl B <petter@axesnet.com>
2013-07-12 07:32:23 +0000

    added grader with extra funcionality

--- grader.js
+++ grader.js
@@ -23,10 +23,11 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program =require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT ="index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLFILE_DEFAULT ="index2.html";
+var CHECKSFILE_DEFAULT = "checks.json";    
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -56,6 +57,18 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlUrl = function(htmlurl, checksurl) {
+    $ = cheerio.load(htmlurl);
+    var checks = loadChecks(checksurl).sort();
+    var out = {};
+    for(var ii in checks) {
+       var present = $(checks[ii]).length > 0;
+       out[checks[ii]] = present;
+    }
+    return out;
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -66,10 +79,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.jason', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u,--url <url>','Url to process')
         .parse(process.argv)
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url){     // validation when url is send  
+      rest.get(program.url).on('complete', function(result){  //restler get the page            
+      var checkJson = checkHtmlUrl(result, program.checks);   // proces the webpage with cherio     
+      console.log(checkJson);				// show the relust process	
+      });
+    }   
+    else
+    {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }  
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
